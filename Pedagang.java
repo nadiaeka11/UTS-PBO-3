5a59f36472bf533fc5fb557a42959b67c4789ff8
nadia.e.f <109936900+nadiaeka11@users.noreply.github.com>
2023-04-10 10:56:08 +0700

    Update Pedagang.java

--- Pedagang.java
+++ Pedagang.java
@@ -1,2 +1,18 @@
-public class Pedagang {
+public class Pedagang extends Penduduk {
+    public Pedagang(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+
+    public int pajak() {
+        return (int) (getPendapatan() * 0.12);
+    }
+
+    public int bayar() {
+        return getPendapatan() - pajak();
+    }
+
+    @Override
+    public String toString() {
+        return "Pedagang: " + getNama() + ", bayar = " + bayar() + ", pajak = " + pajak();
+    }
 }
