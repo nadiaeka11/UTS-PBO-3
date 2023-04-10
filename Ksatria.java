bd29facbf3da2318370495f2d7643770e2ea6964
nadia.e.f <109936900+nadiaeka11@users.noreply.github.com>
2023-04-10 10:55:35 +0700

    Update Ksatria.java

--- Ksatria.java
+++ Ksatria.java
@@ -1,3 +1,18 @@
-public class Ksatria {
+public class Ksatria extends Penduduk {
+    public Ksatria(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
 
+    public int pajak() {
+        return (int) (getPendapatan() * 0.18);
+    }
+
+    public int bayar() {
+        return getPendapatan() - pajak();
+    }
+
+    @Override
+    public String toString() {
+        return "Ksatria: " + getNama() + ", bayar = " + bayar() + ", pajak = " + pajak();
+    }
 }
