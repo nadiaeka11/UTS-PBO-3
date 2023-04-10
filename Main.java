ed46d0433783ad0ad5f0441c3a721cea20b7ff8b
nadia.e.f <109936900+nadiaeka11@users.noreply.github.com>
2023-04-10 11:01:25 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nama");
-        System.out.println("Bayar");
+        
+        Ksatria ksatria = new Ksatria("Joko", 5000000);
+        System.out.println(ksatria.toString());
+        
+        Pedagang pedagang = new Pedagang("Budi", 10000000);
+        System.out.println(pedagang.toString());
+        
+        Petani petani = new Petani("Siti", 2000000);
+        System.out.println(petani.toString()); 
+        
+        PandaiBesi pandaiBesi = new PandaiBesi("Agus", 3000000);
+        System.out.println(pandaiBesi.toString()); 
     }
 }
