538b1c331fea0de74d3e12906231132148234cb2
nadia.e.f <109936900+nadiaeka11@users.noreply.github.com>
2023-04-10 10:58:16 +0700

    Update PandaiBesi.java

--- PandaiBesi.java
+++ PandaiBesi.java
@@ -1,2 +1,18 @@
-public class PandaiBesi {
+public class PandaiBesi extends Penduduk {
+    public PandaiBesi(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+
+    public int pajak() {
+        return (int) (getPendapatan() * 0.06);
+    }
+
+    public int bayar() {
+        return getPendapatan() - pajak();
+    }
+
+    @Override
+    public String toString() {
+        return "Pandai Besi: " + getNama() + ", bayar = " + bayar() + ", pajak = " + pajak();
+    }
 }
