6a73c01e1fff356cb330b574a35b5a7b11bbe40c
nadia.e.f <109936900+nadiaeka11@users.noreply.github.com>
2023-04-10 10:56:35 +0700

    Update Petani.java

--- Petani.java
+++ Petani.java
@@ -1,2 +1,18 @@
-public class Petani {
+public class Petani extends Penduduk {
+    public Petani(String nama, int pendapatan) {
+        super(nama, pendapatan);
+    }
+
+    public int pajak() {
+        return (int) (getPendapatan() * 0.03);
+    }
+
+    public int bayar() {
+        return getPendapatan() - pajak();
+    }
+
+    @Override
+    public String toString() {
+        return "Petani: " + getNama() + ", bayar = " + bayar() + ", pajak = " + pajak();
+    }
 }
